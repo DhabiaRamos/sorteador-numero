c7108e26de0adf54f26824e68c50d3ebeaa5dac6
DhabiaRamos <dhairamosinacio@gmail.com>
2024-03-10 15:07:29 -0300

    arrumando o bug da function do if - primeiro valor ser menor do que o último valor colocado

--- script.js
+++ script.js
@@ -7,16 +7,22 @@ function sortearNum() {
     const inputMin = document.querySelector(".input-numeroInicial")
     const inputMax = document.querySelector(".input-numeroFinal")
 
+    // Verificando se o input está vazio
+
     if (inputMin.value.length == 0 || inputMax.value.length == 0) {
         alert("Preencha os campos com números")
+    }
 
-    } else if (inputMin >= inputMax) {
+    //Transformando para valores númericos
+
+    let min = parseInt(inputMin.value)
+    let max = parseInt(inputMax.value)
+
+    if (min > max) {
         alert("O primeiro valor tem que ser menor que o último valor!")
 
     } else {
 
-        let min = parseInt(inputMin.value)
-        let max = parseInt(inputMax.value)
 
         let gerarNum = Math.floor(Math.random() * (max - min + 1) + min)
 
